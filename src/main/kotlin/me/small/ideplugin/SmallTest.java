6a40963fa1af1ff5351881b26f9b0b8e1b544293
small <smallp010@qq.com>
2022-11-08 17:49:23 +0800

    init

---
+++ src/main/kotlin/me/small/ideplugin/SmallTest.java
@@ -0,0 +1,12 @@
+package me.small.ideplugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class SmallTest extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
